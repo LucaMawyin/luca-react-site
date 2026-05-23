d86f351080e578660b91085f116b9836013cf8d6
LucaMawyin <lucamawyin@gmail.com>
2026-05-22 21:39:11 -0400

    Expanded special words

--- src/lib/titleCaseRules.ts
+++ src/lib/titleCaseRules.ts
@@ -9,21 +9,97 @@ export const minorWords = new Set([
 export const specialWords: Record<string, string> = {
 	javascript: "JavaScript",
 	typescript: "TypeScript",
+
 	nodejs: "Node.js",
 	"node.js": "Node.js",
+
 	reactjs: "ReactJS",
+	react: "React",
+
 	nextjs: "Next.js",
+	"next.js": "Next.js",
+
+	vuejs: "Vue.js",
+	vue: "Vue",
+
+	angularjs: "AngularJS",
+	angular: "Angular",
+
+	svelte: "Svelte",
+	sveltekit: "SvelteKit",
+
 	tailwindcss: "Tailwind CSS",
+	tailwind: "Tailwind CSS",
+
 	html: "HTML",
 	css: "CSS",
+
+	sass: "Sass",
+	scss: "SCSS",
+	less: "Less",
+
 	json: "JSON",
+	xml: "XML",
+	yaml: "YAML",
+
 	sql: "SQL",
+	mysql: "MySQL",
+	postgresql: "PostgreSQL",
+	postgres: "Postgres",
+	mongodb: "MongoDB",
+	sqlite: "SQLite",
+
 	graphql: "GraphQL",
+	rest: "REST",
+	api: "API",
+
 	php: "PHP",
 	python: "Python",
 	rust: "Rust",
 	go: "Go",
+	golang: "Go",
+	java: "Java",
+	kotlin: "Kotlin",
+	swift: "Swift",
+	csharp: "C#",
+	"c#": "C#",
+	cpp: "C++",
+	"c++": "C++",
+	c: "C",
+
+	dart: "Dart",
+
 	docker: "Docker",
 	kubernetes: "Kubernetes",
+	k8s: "Kubernetes",
+
 	github: "GitHub",
+	gitlab: "GitLab",
+	git: "Git",
+
+	npm: "npm",
+	yarn: "Yarn",
+	pnpm: "pnpm",
+
+	expressjs: "Express.js",
+	express: "Express",
+
+	nestjs: "NestJS",
+	deno: "Deno",
+	bun: "Bun",
+
+	firebase: "Firebase",
+	supabase: "Supabase",
+
+	vercel: "Vercel",
+	netlify: "Netlify",
+
+	redis: "Redis",
+	elasticsearch: "Elasticsearch",
+
+	linux: "Linux",
+	windows: "Windows",
+	macos: "macOS",
+	ios: "iOS",
+	android: "Android",
 };
