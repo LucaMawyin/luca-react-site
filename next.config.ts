b5dff959d41d65991927d91f1d333d06502c0337
lucamawyin <lucamawyin@gmail.com>
2026-06-03 17:08:44 -0400

    test

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
+    async rewrites() {
+        return [
+            {
+                source: "/resume",
+                destination: "/resume/resume.pdf",
+            },
+        ];
+    },
 };
 
 
