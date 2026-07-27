cb24fe36ec6657d61068e754b4b34ab1b231ee98
LucaMawyin <lucamawyin@gmail.com>
2026-07-27 10:12:17 -0400

    Removed: api/cron Added: worker.ts file with cron trigger for clearing sessions

---
+++ worker.ts
@@ -0,0 +1,24 @@
+// @ts-ignore
+import { default as handler } from "./.open-next/worker.js";
+import { getDB } from "./src/lib/db";
+
+export default {
+    fetch: handler.fetch,
+
+    async scheduled(
+        event: ScheduledEvent,
+        env: any,
+        ctx: ExecutionContext
+    ) {
+        ctx.waitUntil(
+            (async () => {
+                const db = await getDB();
+
+                await db.prepare(`
+                    DELETE FROM sessions
+                    WHERE expires_at < datetime('now')
+                `).run();
+            })()
+        );
+    },
+};
