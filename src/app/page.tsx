aeff3405d98c138fd8e64ad2e1560926ac7e06e8
LucaMawyin <lucamawyin@gmail.com>
2026-08-01 11:08:56 -0400

    temporarily remove projects from /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,17 @@
 import { validateSession } from "@/lib/auth";
 import HomeClient from "./HomeClient";
 import { getProjects, getTech } from "@/lib/getProjects";
-import { Session } from "@/lib/types";
+import { Project, Session } from "@/lib/types";
 import { getContent } from "@/lib/getContent";
 import { getExperience } from "@/lib/getExperience";
 
 export default async function Home() {
 
     const session = await validateSession() as Session;
-
-    console.log("SESSION DONE");
-
-    const projects = await getProjects(session);
-
-    console.log("PROJECTS DONE");
-
+    const projects = [] as Project[];
     const tech = await getTech();
-
-    console.log("TECH DONE");
-
     const about = await getContent();
-
-    console.log("CONTENT DONE");
-
     const experience = await getExperience();
-
-    console.log("EXPERIENCE DONE");
 
     return (
         <HomeClient 
