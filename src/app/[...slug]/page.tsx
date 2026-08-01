2bc31fb78e50836db1b892c04e480c3425f7a67c
LucaMawyin <lucamawyin@gmail.com>
2026-08-01 13:53:27 -0400

    run gets in sequence

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -8,18 +8,10 @@ import { getExperience } from "@/lib/getExperience";
 export default async function Home() {
 
     const session = await validateSession() as Session;
-
-    const [
-        projects,
-        tech,
-        about,
-        experience
-    ] = await Promise.all([
-        getProjects(session),
-        getTech(),
-        getContent(),
-        getExperience()
-    ]);
+    const projects = await getProjects(session);
+    const tech = await getTech();
+    const about = await getContent();
+    const experience = await getExperience();
 
     return (
         <HomeClient 
