0decb00d86318d3291e00f99a6867106470c1e9b
LucaMawyin <lucamawyin@gmail.com>
2026-08-01 11:11:20 -0400

    Added back getProjects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import { validateSession } from "@/lib/auth";
 import HomeClient from "./HomeClient";
 import { getProjects, getTech } from "@/lib/getProjects";
-import { Project, Session } from "@/lib/types";
+import { Session } from "@/lib/types";
 import { getContent } from "@/lib/getContent";
 import { getExperience } from "@/lib/getExperience";
 
 export default async function Home() {
 
     const session = await validateSession() as Session;
-    const projects = [] as Project[];
+    const projects = await getProjects(session);
     const tech = await getTech();
     const about = await getContent();
     const experience = await getExperience();
