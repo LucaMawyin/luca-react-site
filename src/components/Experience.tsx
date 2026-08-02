4547ee0c90edc07002277436a5cf7725c705901e
LucaMawyin <lucamawyin@gmail.com>
2026-08-02 15:45:26 -0400

    Staggered experience cards

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -48,140 +48,162 @@ export default function ExperienceClient(props: { isLoggedIn:boolean; experience
 
             {/* CARDS */}
             <div className={`
-                md:w-[50vw]
+                relative
                 w-full
                 grid
-                items-stretch
-                auto-rows-fr
                 
                 p-[5%]
+                px-[2.5%]
                 ${props.isLoggedIn ? "" : "sm:gap-16"}
                 gap-8
-                relative
             `}>
 
-                {/* VERTICAL LINE */}
+                {/* CENTER LINE */}
                 <div className="
                     absolute
-                    left-0 md:left-6
-                    top-0 bottom-0
+                    hidden md:block
+                    left-1/2
+                    top-8 bottom-0
                     w-0.5
                     bg-neutral-300
-                    pointer-events-none
-                    z-0
+                    -translate-x-1/2
                 " />
 
                 {experience.map((exp,index) => (
                     <FadeInOnView 
                         key={index}
-                        className="
-                            w-full 
-                            flex flex-col 
-                            items-center 
+                        className={`
+                            relative
+                            flex 
+                            w-full
+                            lg:px-[5%]
+
                             gap-8 
-                            justify-between
-                        "
+                            ${index % 2 === 0 
+                                ? "md:justify-start" 
+                                : "md:justify-end"
+                            }
+                        `}
                     >
-                        
-                        <div
-                            className="
-                                flex
-                                flex-1
-                                flex-col
-                                rounded-xl
-                                shadow-[0_4px_10px_rgba(0,0,0,0.08),0_-1px_3px_rgba(0,0,0,0.04)]
-                                w-full
-                                p-6
-                                gap-4
-                            "
-                        >
-                            {/* TITLE + DATE */}
-                            <div className="flex flex-col sm:flex-row sm:items-center sm:gap-3">
-                                <h2 className="text-lg sm:text-xl font-semibold">
-                                    {exp.title}
-                                </h2>
+                        {/* DOT */}
+                        <div className="
+                            hidden md:block
+                            absolute
+                            left-1/2
+                            top-1/2
+                            w-4 h-4
+                            rounded-full
+                            bg-neutral-400
+                            -translate-x-1/2
+                            -translate-y-1/2
+                            z-10
+                        " />
 
-                                <span className="hidden sm:inline text-neutral-400">•</span>
+                        <div className="
+                            flex
+                            flex-col
+                            gap-8
+                            w-full
+                            md:w-[45%]
+                        ">
+                            <div
+                                className="
+                                    flex
+                                    flex-col
+                                    rounded-xl
+                                    shadow-[0_4px_10px_rgba(0,0,0,0.08),0_-1px_3px_rgba(0,0,0,0.04)]
+                                    p-6
+                                    gap-4
+                                "
+                            >
+                                {/* TITLE + DATE */}
+                                <div className="flex flex-col sm:flex-row sm:items-center sm:gap-3">
+                                    <h2 className="text-lg sm:text-xl font-semibold">
+                                        {exp.title}
+                                    </h2>
 
-                                <span className="text-sm text-neutral-500">
-                                    {formatToMonthYear(exp.start_date)} - {exp.end_date ? formatToMonthYear(exp.end_date) : "Present"}
-                                </span>
-                            </div>
+                                    <span className="hidden sm:inline text-neutral-400">•</span>
 
-                            {/* COMPANY + LOCATION */}
-                            <div className="flex flex-col sm:flex-row sm:items-center sm:gap-3">
-                                <span className="text-sm">
-                                    {exp.company}
-                                </span>
-
-                                <span className="hidden sm:inline text-neutral-400">|</span>
-
-                                <span className="text-sm text-neutral-500">
-                                    {exp.city}, {exp.region}
-                                </span>
-                            </div>
-
-                            {/* TAG */}
-                            {exp.tag && (
-                                <div>
-                                    <span className="text-xs bg-neutral-100 text-neutral-600 px-2 py-1 rounded-full">
-                                        {exp.tag}
+                                    <span className="text-sm text-neutral-500">
+                                        {formatToMonthYear(exp.start_date)} - {exp.end_date ? formatToMonthYear(exp.end_date) : "Present"}
                                     </span>
                                 </div>
-                            )}
-                            
-                            <div className="text-sm mt-2">
-                                <ReactMarkdown
-                                    components={{
-                                        ul: ({ ...props }) => (
-                                            <ul className="list-disc pl-5 space-y-1" {...props} />
-                                        ),
-                                        li: ({ ...props }) => (
-                                            <li {...props} />
-                                        ),
-                                    }}
-                                    >
-                                    {exp.description}
-                                </ReactMarkdown>
+
+                                {/* COMPANY + LOCATION */}
+                                <div className="flex flex-col sm:flex-row sm:items-center sm:gap-3">
+                                    <span className="text-sm">
+                                        {exp.company}
+                                    </span>
+
+                                    <span className="hidden sm:inline text-neutral-400">|</span>
+
+                                    <span className="text-sm text-neutral-500">
+                                        {exp.city}, {exp.region}
+                                    </span>
+                                </div>
+
+                                {/* TAG */}
+                                {exp.tag && (
+                                    <div>
+                                        <span className="text-xs bg-neutral-100 text-neutral-600 px-2 py-1 rounded-full">
+                                            {exp.tag}
+                                        </span>
+                                    </div>
+                                )}
+                                
+                                <div className="text-sm mt-2">
+                                    <ReactMarkdown
+                                        components={{
+                                            ul: ({ ...props }) => (
+                                                <ul className="list-disc pl-5 space-y-1" {...props} />
+                                            ),
+                                            li: ({ ...props }) => (
+                                                <li {...props} />
+                                            ),
+                                        }}
+                                        >
+                                        {exp.description}
+                                    </ReactMarkdown>
+                                </div>
+
                             </div>
 
-                        </div>
-
-                        {/* Delete button if logged in */}
-                        {props.isLoggedIn && (
-                            <div className="flex h-fit w-full justify-between">
-                                <Button
-                                    text="Edit"
-                                    className="min-w-32 h-fit"
-                                    onClick={() => {router.push(`add-experience/edit?id=${exp.id}`)}}
-                                />
-                                <DeleteButton
-                                    className="min-w-32 h-fit"
-                                    text="Experience"
-                                    action={async () => {
-                                    const res = await fetch("/api/experience", {
-                                        method: "DELETE",
-                                        headers: {
-                                            "Content-Type": "application/json",
-                                        },
-                                        body: JSON.stringify({ id: exp.id }),
-                                    });
+                            {/* Delete button if logged in */}
+                            {props.isLoggedIn && (
+                                <div className="flex h-fit w-full justify-between">
+                                    <Button
+                                        text="Edit"
+                                        className="min-w-32 h-fit"
+                                        onClick={() => {router.push(`add-experience/edit?id=${exp.id}`)}}
+                                    />
+                                    <DeleteButton
+                                        className="min-w-32 h-fit"
+                                        text="Experience"
+                                        action={async () => {
+                                        const res = await fetch("/api/experience", {
+                                            method: "DELETE",
+                                            headers: {
+                                                "Content-Type": "application/json",
+                                            },
+                                            body: JSON.stringify({ id: exp.id }),
+                                        });
+            
+                                        if (res.status === 401) {
+                                            router.push("/login");
+                                            return;
+                                        }
+                                        
+                                            setExperience((prev) =>
+                                                prev.filter((p) => p.id !== exp.id)
+                                            );
+                                        }}
+                                    />                        
+                                </div>
         
-                                    if (res.status === 401) {
-                                        router.push("/login");
-                                        return;
-                                    }
-                                    
-                                        setExperience((prev) =>
-                                            prev.filter((p) => p.id !== exp.id)
-                                        );
-                                    }}
-                                />                        
-                            </div>
-    
-    
-                        )}
-
+        
+                            )}
+                        </div>
+                        
                     </FadeInOnView>
                     
                 ))}
